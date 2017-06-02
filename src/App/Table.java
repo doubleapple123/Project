d5653b11937585902e606762adb778509fc81587
doubleapple123 <leonychen123@gmail.com>
2017-06-02 11:22:33 -0700

    a

--- src/App/Table.java
+++ src/App/Table.java
@@ -3,7 +3,7 @@ package App;
 import javax.swing.*;
 import java.awt.*;
 
-
+//https://stackoverflow.com/questions/13833688/adding-jbutton-to-jtable
 /**
  * Created by Leon Chen on 5/26/2017.
  */
@@ -11,16 +11,19 @@ public class Table {
     public static void main(String[] args) {
         Run obj = new Run("TableTest");
 
-        String [] col = {"Game","Date", "Time", "Network"};
-        Object [][] data = {{"Thunder at 76ers","10/26/16","8:00 PM","ESPN"},{"Rockets at Lackers", "10/26/16","10:30", "ESPN"}};
+        String [] col = {"This","Game","Date", "Time", "Network"};
+        Object [][] data = {{"Thunder at 76ers","10/26/16","8:00 PM","ESPN"}
+        ,{"Rockets at Lackers", "10/26/16","10:30", "ESPN"}};
 
         JTable tab = new JTable(data,col);
 
         JPanel pane = new JPanel();
+
         tab.setFillsViewportHeight(true);
         pane.setLayout(new BorderLayout());
         pane.add(tab.getTableHeader(), BorderLayout.PAGE_START);
         pane.add(tab, BorderLayout.CENTER);
+
         obj.add(pane);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.setVisible(true);
